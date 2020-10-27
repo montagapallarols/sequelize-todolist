02e953090d4ef566b0b99a7ab2fdec30b693c7c1
montipallarols <montaga.pallarols@gmail.com>
2020-10-27 18:34:15 +0100

    Run migration so new column gets added to table

--- migrations/20201027171338-set-up-relations.js
+++ migrations/20201027171338-set-up-relations.js
@@ -2,20 +2,21 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.addColumn("todoLists", "userId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn("todoLists", "userId");
+    //  * Example:
+    //  * await queryInterface.dropTable('users');
+    //  */
   }
 };
