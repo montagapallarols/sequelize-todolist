734bd549d8f44f749fee62ab78451a719ef0e296
montipallarols <montaga.pallarols@gmail.com>
2020-10-27 16:13:14 +0100

    Create queries file and add different functions

---
+++ queries.js
@@ -0,0 +1,70 @@
+const User = require("./models").user;
+const TodoItem = require("./models").todoItem;
+
+async function findAllUsers() {
+    try {
+      const allUsers = await User.findAll();
+      return allUsers.map(user => user.get({plain: true}))
+      
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  
+  findAllUsers().then(allUsers => console.log(allUsers));
+
+  async function findAllTodoItems(){
+      try {
+        const allTodoItems = await TodoItem.findAll();
+        return allTodoItems.map(item => item.get({plain: true}))
+
+      } catch(e) {
+          console.error(e);
+      }
+  }
+
+  findAllTodoItems().then(todos => console.log(todos));
+
+
+  async function findUserByPk(key){
+      try {
+        const userByPk = await User.findByPk(key);
+      return userByPk;
+      }  catch(e) {
+          console.error(e)
+      }  
+  }
+
+  findUserByPk(1).then(user => console.log(user))
+
+
+  async function createNewUser(){
+      try {
+        const newUser = await User.create({
+            name: "John Frusciante",
+            email: "john@john",
+            phone: 54321,
+            password: "test"
+        });
+        return newUser;
+      } catch(e) {
+          console.error(e)
+      }
+  }
+
+//   createNewUser().then(newUser => console.log(newUser))
+
+async function searchImportantTodoItems(){
+    try {
+        const importantTodoItems = await TodoItem.findAll({ where: { important: true } });
+        return importantTodoItems.map(item => item.get({plain: true}))
+    } catch(e) {
+        console.error(e)
+    }
+}
+
+searchImportantTodoItems().then(important => console.log(important))
+
+
+
+
